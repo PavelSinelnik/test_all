ee5fd21d55079d1f524f6bac7a63ba8e7859357d
pavel11111111111111111111 <55051040+pavel11111111111111111111@users.noreply.github.com>
2020-03-26 12:55:45 +0300

    Add files via upload

--- check_device_info.py
+++ check_device_info.py
@@ -1,15 +1,24 @@
+import json
 import boto3
-
-client = boto3.client('s3')
-
-
+import zipfile
 def lambda_handler(event, context):
-    list_of_files = client.list_objects(
-        Bucket='example141323'
-    )
-    for i in list_of_files['Contents']:
-        if i['Key'] == event['id']:
-            return {
-                "Status code ": "200"
-            }
-    return "Not Found n"
+    s3=boto3.client('s3')
+    l=boto3.client('lambda')
+    #key = event["Records"][0]['s3']['object']['key']
+   #bucket = event['Records'][0]['s3']['bucket']['name']
+    #if key == 'save_device_info.zip':
+        #name='save_device_info'
+    #if key == 'check_device_info.zip':
+        #name = 'check_device_info'
+    
+    #if name == 'save_device_info' or name == 'check_device_info':
+    l.update_function_code(
+            FunctionName="check_device_info",
+            S3Bucket=forevent,
+            S3Key="save_device_info"
+                )
+    #item = json.dumps(event['Records'][0])
+    #object = s3.put_object(Bucket="forevent", Key='tap1.json', Body=item)
+    
+    return('neeeeeeeeeeeeeeee')
+    
